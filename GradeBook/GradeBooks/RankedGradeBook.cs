bbf27e526b67f61464a723428f05a4a88b9a1907
Konrad Barszcz <konrad.barszcz@microsoft.wsei.edu.pl>
2022-03-08 00:08:53 +0100

    Override CalculateStatistics Method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -26,5 +26,16 @@ namespace GradeBook.GradeBooks
             return 'F';
 
         }
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+            else
+            {
+                base.CalculateStatistics();
+            }
+        }
     }
 }
