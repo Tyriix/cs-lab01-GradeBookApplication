ad05d35eb57bf08273ce7f2d12799c5706a0016c
Konrad Barszcz <konrad.barszcz@microsoft.wsei.edu.pl>
2022-03-08 00:07:23 +0100

    Override GetLetterGrade Method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -9,5 +9,22 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
         }
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+            if (averageGrade >= 80)
+                return 'A';
+            if (averageGrade >= 60)
+                return 'B';
+            if (averageGrade >= 40)
+                return 'C';
+            if (averageGrade >= 20)
+                return 'D';
+            return 'F';
+
+        }
     }
 }
